0d58a5e4249868f7bf60160da6ad4f978921bbce
Efim Zabarsky <[Efim.Zabarsky@kla-tencor.com]>
2024-09-14 14:31:58 +0300

    add wifi connection

--- TemperatureIOT/NFAppTemperaturePressureAzureIoT/NFAppTemperaturePressureAzureIoT/Program.cs
+++ TemperatureIOT/NFAppTemperaturePressureAzureIoT/NFAppTemperaturePressureAzureIoT/Program.cs
@@ -1,20 +1,78 @@
+using nanoFramework.Networking;
+using nanoFramework.Logging;
 using System;
-using System.Diagnostics;
+using System.Device.Gpio;
 using System.Threading;
+using nanoFramework.Logging.Serial;
+using Microsoft.Extensions.Logging;
+using System.IO.Ports;
+using nanoFramework.Hardware.Esp32;
 
 namespace NFAppTemperaturePressureAzureIoT
 {
 	public class Program
 	{
+		private static int sleepTimeMilliseconds = 60000;
+		private static GpioController s_GpioController;
+		private static SerialLogger logger = null;
+		private static Secrets secrets = new Secrets();
+		private static readonly int minutesToGoToSleep = 2;
+
 		public static void Main()
 		{
-			Debug.WriteLine("Hello from nanoFramework!");
+			Init();
+			new Thread(Blink).Start();
+			ConnectToWifi();
+		}
 
-			Thread.Sleep(Timeout.Infinite);
+		private static void Init()
+		{
+			SerialPort serial = new(secrets.ComPort);
+			serial.BaudRate = 115200;
+			logger = new SerialLogger(ref serial, "My logger");
+			logger.MinLogLevel = LogLevel.Debug;
+			Trace("Program Started, connecting to Wifi.");
+		}
 
-			// Browse our samples repository: https://github.com/nanoframework/samples
-			// Check our documentation online: https://docs.nanoframework.net/
-			// Join our lively Discord community: https://discord.gg/gCyBu8T
+		private static void ConnectToWifi()
+		{
+
+			CancellationTokenSource cs = new(sleepTimeMilliseconds);
+			var success = NetworkHelper.ConnectWifiDhcp(secrets.Ssid, secrets.Password, setDateTime: true, token: cs.Token);
+			if (!success)
+			{
+				Trace($"Can't connect to wifi: {NetworkHelper.ConnectionError.Error}");
+				if (NetworkHelper.ConnectionError.Exception != null)
+				{
+					Trace($"NetworkHelper.ConnectionError.Exception");
+				}
+
+				GoToSleep();
+			}
+		}
+
+		private static void GoToSleep()
+		{
+			Sleep.EnableWakeupByTimer(new TimeSpan(0, 0, minutesToGoToSleep, 0));
+			Sleep.StartDeepSleep();
+		}
+
+		static void Trace(string message)
+		{
+			logger?.LogDebug(message);
+		}
+
+		private static void Blink()
+		{
+			s_GpioController = new GpioController();
+			GpioPin led = s_GpioController.OpenPin(2, PinMode.Output);
+			led.Write(PinValue.Low);
+
+			while (true)
+			{
+				led.Toggle();
+				Thread.Sleep(1000);
+			}
 		}
 	}
 }
