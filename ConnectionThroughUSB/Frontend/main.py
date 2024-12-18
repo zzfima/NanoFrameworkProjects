e25f24ad670bb9263e9e823354425ae56796f780
Efim Zabarsky <[Efim.Zabarsky@kla-tencor.com]>
2024-12-18 10:51:57 +0200

    rfct

--- ConnectionThroughUSB/Frontend/main.py
+++ ConnectionThroughUSB/Frontend/main.py
@@ -8,7 +8,6 @@ root.title("ESP32 Command Sender")
 portName = tk.StringVar()
 ser = None
 
-
 def send_command_set_port():
     global ser
     ser = serial.Serial(portName.get(), 115200, timeout=1)
@@ -16,21 +15,17 @@ def send_command_set_port():
     button_b.config(state=tk.NORMAL)
     button_c.config(state=tk.NORMAL)
 
-
 def send_command_a():
     if ser:
         ser.write(b"a\n")
-
 
 def send_command_b():
     if ser:
         ser.write(b"b\n")
 
-
 def send_command_c():
     if ser:
         ser.write(b"c\n")
-
 
 lbl_port = tk.Label(root, text="Edit port name for init:")
 lbl_port.pack(pady=5)
@@ -40,19 +35,13 @@ name_entry.pack(pady=5)
 button_port = tk.Button(root, text="set port", command=send_command_set_port)
 button_port.pack(pady=5)
 
-button_a = tk.Button(
-    root, text="Close the circuit", command=send_command_a, state=tk.DISABLED
-)
+button_a = tk.Button(root, text="Close the circuit", command=send_command_a, state=tk.DISABLED)
 button_a.pack(pady=20)
 
-button_b = tk.Button(
-    root, text="Open the circuit", command=send_command_b, state=tk.DISABLED
-)
+button_b = tk.Button(root, text="Open the circuit", command=send_command_b, state=tk.DISABLED)
 button_b.pack(pady=20)
 
-button_c = tk.Button(
-    root, text="blink three times", command=send_command_c, state=tk.DISABLED
-)
+button_c = tk.Button(root, text="blink three times", command=send_command_c, state=tk.DISABLED)
 button_c.pack(pady=20)
 
 root.mainloop()
