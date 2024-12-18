684041f65b2f5776a023f9e6952646ba1b90aa4d
Efim Zabarsky <[Efim.Zabarsky@kla-tencor.com]>
2024-12-18 09:57:17 +0200

    add input box

--- ConnectionThroughUSB/Frontend/main.py
+++ ConnectionThroughUSB/Frontend/main.py
@@ -1,29 +1,49 @@
 import serial
 import tkinter as tk
 
-# Configure the serial port (adjust 'COM7' to your actual COM port)
-ser = serial.Serial('COM7', 115200, timeout=1)
-
-def send_command_a():
-    ser.write(b'a\n')
-
-def send_command_b():
-    ser.write(b'b\n')
-    
-def send_command_c():
-    ser.write(b'c\n')
-
 # Create the GUI
 root = tk.Tk()
 root.title("ESP32 Command Sender")
 
-button_a = tk.Button(root, text="Send 'a'", command=send_command_a)
+portName = tk.StringVar()
+ser = None
+
+
+def send_command_set_port():
+    global ser
+    ser = serial.Serial(portName.get(), 115200, timeout=1)
+
+
+def send_command_a():
+    if ser:
+        ser.write(b"a\n")
+
+
+def send_command_b():
+    if ser:
+        ser.write(b"b\n")
+
+
+def send_command_c():
+    if ser:
+        ser.write(b"c\n")
+
+
+lbl_port = tk.Label(root, text="Edit port name for init:")
+lbl_port.pack(pady=5)
+name_entry = tk.Entry(root, textvariable=portName, font=("calibre", 10, "normal"))
+name_entry.insert(0, "COM0")
+name_entry.pack(pady=5)
+button_port = tk.Button(root, text="set port", command=send_command_set_port)
+button_port.pack(pady=5)
+
+button_a = tk.Button(root, text="blink one time", command=send_command_a)
 button_a.pack(pady=20)
 
-button_b = tk.Button(root, text="Send 'b'", command=send_command_b)
+button_b = tk.Button(root, text="blink two times", command=send_command_b)
 button_b.pack(pady=20)
 
-button_c = tk.Button(root, text="Send 'c'", command=send_command_c)
+button_c = tk.Button(root, text="blink three times", command=send_command_c)
 button_c.pack(pady=20)
 
 root.mainloop()
