9aacc207b5fc2870e9dfc9a965ce9e0fbdb1f9cb
Efim Zabarsky <[Efim.Zabarsky@kla-tencor.com]>
2024-12-29 11:03:17 +0200

    new file

--- ConnectionThroughUSB/Arduino/serial/serial.ino
+++ ConnectionThroughUSB/Arduino/serial/serial.ino
@@ -2,19 +2,42 @@
 #define BAUD_RATE 115200
 String receivedMessage = "";  // Variable to store the complete message
 
+void Blink(int frequencyMs) {
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(frequencyMs);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(frequencyMs);
+}
+
+void printStartMessages() {
+  Blink(2000);
+
+  Serial.println("Welcome to relay commander");
+
+  Serial.println("Commands list:");
+  Serial.println("Door1.Interlock.On");
+  Serial.println("Door1.Interlock.Off");
+  Serial.println("Door2.Interlock.On");
+  Serial.println("Door2.Interlock.Off");
+  Serial.println("Door3.Interlock.On");
+  Serial.println("Door3.Interlock.Off");
+
+  Serial.println("LP1.Interlock.On");
+  Serial.println("LP1.Interlock.Off");
+  Serial.println("LP2.Interlock.On");
+  Serial.println("LP2.Interlock.Off");
+
+  Blink(2000);
+}
+
 // the setup function runs once when you press reset or power the board
 void setup() {
   // initialize digital pin LED_BUILTIN as an output.
   pinMode(LED_BUILTIN, OUTPUT);
   // Start the Serial Monitor at a baud rate of 115200
   Serial.begin(BAUD_RATE);
-}
 
-void Blink(int frequencyMs) {
-  digitalWrite(LED_BUILTIN, HIGH);
-  delay(frequencyMs);
-  digitalWrite(LED_BUILTIN, LOW);
-  delay(frequencyMs);
+  printStartMessages();
 }
 
 // the loop function runs over and over again forever
