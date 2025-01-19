53f7580ec65b6444b118ead3a777ad2287ee8a92
zzfima <zzfima@gmail.com>
2025-01-19 10:19:16 -0800

    rfct

--- ArduinoEsp32Mini/ClockOled/sketch_jan19a/sketch_jan19a.ino
+++ ArduinoEsp32Mini/ClockOled/sketch_jan19a/sketch_jan19a.ino
@@ -1,6 +1,5 @@
 //ESP32 mini c3 SDA, SCL pins are: 8 and 9
 
-
 #include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
@@ -11,26 +10,27 @@
 #define OLED_RESET     -1 // Reset pin # (or -1 if sharing Arduino reset pin)
 #define SCREEN_ADDRESS 0x3C //0x3D ///< See datasheet for Address; 0x3D for 128x64, 0x3C for 128x32
 
-#defin SDA_PIN = 8;
-#defin SCL_PIN = 9;
+#define SDA_PIN 8
+#define SCL_PIN 9
 
 // Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
 void setup() {
-  Serial.begin(115200);
   Wire.begin(SDA_PIN, SCL_PIN);    
-
+/*
+  Serial.begin(115200);
   if(!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
     Serial.println(F("SSD1306 allocation failed"));
     for(;;);
   }
+*/
   delay(2000);
   display.clearDisplay();
 
   display.setTextSize(1);
   display.setTextColor(WHITE);
-  display.setCursor(0, 10);
+  display.setCursor(10, 10);
   // Display static text
   display.println("Hello, world!");
   display.display(); 
