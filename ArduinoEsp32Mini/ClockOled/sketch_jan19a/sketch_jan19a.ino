92dba98800d517b3e5cc8f398c8026451cec3197
Efim Zabarsky <[Efim.Zabarsky@kla-tencor.com]>
2025-01-19 15:19:18 +0200

    add clock for esp32 mini

---
+++ ArduinoEsp32Mini/ClockOled/sketch_jan19a/sketch_jan19a.ino
@@ -0,0 +1,42 @@
+//ESP32 mini c3 SDA, SCL pins are: 8 and 9
+
+
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+
+#define OLED_RESET     -1 // Reset pin # (or -1 if sharing Arduino reset pin)
+#define SCREEN_ADDRESS 0x3C //0x3D ///< See datasheet for Address; 0x3D for 128x64, 0x3C for 128x32
+
+#defin SDA_PIN = 8;
+#defin SCL_PIN = 9;
+
+// Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
+void setup() {
+  Serial.begin(115200);
+  Wire.begin(SDA_PIN, SCL_PIN);    
+
+  if(!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;);
+  }
+  delay(2000);
+  display.clearDisplay();
+
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.setCursor(0, 10);
+  // Display static text
+  display.println("Hello, world!");
+  display.display(); 
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
